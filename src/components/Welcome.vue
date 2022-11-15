9c36c80a526f0f87bf71b20b04a1e865e6760560
David Araújo <david2araujo5@gmail.com>
2022-11-15 18:02:31 +0000

    Update

---
+++ src/components/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Eye />
+        <h1>NetEye</h1>
+        <h2>Software Defined Network monitoring made easy</h2>
+    </div>
+</template>
+
+<script>
+import {Eye} from '@vicons/fa'
+export default {
+    components: {
+        Eye   
+    }
+}
+</script>
+
+<style scoped>
+
+    template {
+        text-align: center;
+    }
+
+</style>
