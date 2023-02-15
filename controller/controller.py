989159cd9bf20a5a174e1ae0daf824ee02c0bb78
David Araújo <david2araujo5@gmail.com>
2023-02-15 18:38:15 +0000

    Need to change controller so that it only needs a .p4 and it handles the compilation

---
+++ controller/controller.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import grpc
+import argparse
+from time import sleep
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='P4Runtime Controller')
+    parser.add_argument('--p4info', help='p4info proto in text format from p4c',
+                        type=str, action="store", required=False,
+                        default='./build/advanced_tunnel.p4.p4info.txt')
+    parser.add_argument('--bmv2-json', help='BMv2 JSON file from p4c',
+                        type=str, action="store", required=False,
+                        default='./build/advanced_tunnel.json')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.p4info):
+        parser.print_help()
+        print("\np4info file not found: %s\nHave you run 'make'?" % args.p4info)
+        parser.exit(1)
+    if not os.path.exists(args.bmv2_json):
+        parser.print_help()
+        print("\nBMv2 JSON file not found: %s\nHave you run 'make'?" % args.bmv2_json)
+        parser.exit(1)
+    main(args.p4info, args.bmv2_json)
