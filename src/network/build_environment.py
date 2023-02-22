a8746831bbe4b396ca88a6baafb4035917d486bf
David Araújo <david2araujo5@gmail.com>
2023-02-22 10:13:56 +0000

    Clean up

--- src/network/build_environment.py
+++ src/network/build_environment.py
@@ -1,13 +1,11 @@
 import argparse
 import json
 import os
-import subprocess
 from time import sleep
 
 from mininet.cli import CLI
 from mininet.link import TCLink
 from mininet.net import Mininet
-from mininet.topo import Topo
 
 from p4_host import P4Host
 from p4runtime_switch import P4RuntimeSwitch
