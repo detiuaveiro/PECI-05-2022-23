635a69dc24091ee4e097fce65746af8f49ba046b
David Araujo <david2araujo5@gmail.com>
2023-02-22 00:57:33 +0000

    Communication working

---
+++ src/network/mininet_topo.py
@@ -0,0 +1,46 @@
+from mininet.topo import Topo
+
+class RunnerTopo(Topo):
+    """ The mininet topology class for the P4 tutorial exercises.
+    """
+    def __init__(self, hosts, switches, links, log_dir, **opts):
+        Topo.__init__(self, **opts)
+        host_links = []
+        switch_links = []
+
+        # assumes host always comes first for host<-->switch links
+        for link in links:
+            if link['node1'][0] == 'h':
+                host_links.append(link)
+            else:
+                switch_links.append(link)
+
+        for sw, _ in switches.items():   
+            self.addSwitch(sw, log_file="%s/%s.log" %(log_dir, sw), cls=None)
+
+        for link in host_links:
+            host_name = link['node1']
+            sw_name, sw_port = self.parse_switch_node(link['node2'])
+            host_ip = hosts[host_name]['ip']
+            host_mac = hosts[host_name]['mac']
+            self.addHost(host_name, ip=host_ip, mac=host_mac)
+            self.addLink(host_name, sw_name,
+                         delay=link['latency'], bw=link['bandwidth'],
+                         port2=sw_port)
+
+        for link in switch_links:
+            sw1_name, sw1_port = self.parse_switch_node(link['node1'])
+            sw2_name, sw2_port = self.parse_switch_node(link['node2'])
+            self.addLink(sw1_name, sw2_name,
+                        port1=sw1_port, port2=sw2_port,
+                        delay=link['latency'], bw=link['bandwidth'])
+
+
+    def parse_switch_node(self, node):
+        assert(len(node.split('-')) == 2)
+        sw_name, sw_port = node.split('-')
+        try:
+            sw_port = int(sw_port[1:])
+        except:
+            raise Exception('Invalid switch node in topology file: {}'.format(node))
+        return sw_name, sw_port
