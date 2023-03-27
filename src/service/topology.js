967f47a70af08c38cd56cf7d10bb8f70bdc2a866
Guilherme Craveiro <gjscraveiro@ua.pt>
2023-03-27 21:31:36 +0100

    backend call update

--- src/service/topology.js
+++ src/service/topology.js
@@ -2,14 +2,14 @@ export default class Topology{
     
     getTopo = () =>
     {
-        var Url = "http://localhost:8000/api/Topology";
+        var Url = "http://backend:8000/api/Topology";
         return fetch(Url)
             .then(response => response.json())
             .then(data => {return data});
     }
     getId = (Id) =>
     {
-        var Url = "http://localhost:8000/api/Topology/" + Id;
+        var Url = "http://backend:8000/api/Topology/" + Id;
         return fetch(Url)
             .then(response => response.json())
             .then(data => {return data});
