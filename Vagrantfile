0c6bbf703b527339fcd6a12a1e144f1c69a1098e
David Araújo <david2araujo5@gmail.com>
2023-04-13 00:05:01 +0100

    Creating interfaces

--- Vagrantfile
+++ Vagrantfile
@@ -1,13 +1,15 @@
 Vagrant.configure("2") do |config|
     config.vm.box = "ubuntu/focal64"
-    config.disksize.size = "10GB"                                   # Plugin install: vagrant plugin install vagrant-disksize
+    config.vm.box_url = "https://app.vagrantup.com/ubuntu/boxes/focal64"
+
+    config.vm.disk :disk, size: "10GB"
 
     config.vm.provision :shell, path: "bootstrap.sh"                # Static. Will run the initial setup when the VM is deployed
     
     config.vm.hostname = "s1"                                       # This should be configured programatically
     
     config.vm.network :forwarded_port, guest: 80, host: 8998        # This should be configured programatically, only the host port will need to be changed
-    #config.vm.network "private_network",                           # This should be configured programatically
-    #    ip: "192.168.53.2",
-    #    netmask: "24"
+    config.vm.network :private_network,                             # This should be configured programatically
+        ip: "192.168.53.2",
+        netmask: "24"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
+for idx in 0 1 2 3 4 5 6 7 8; do
+    intf0="veth$(($idx*2))"
+    intf1="veth$(($idx*2+1))"
+    if ! ip link show $intf0 &> /dev/null; then
+        sudo ip link add name $intf0 type veth peer name $intf1
+        sudo ip link set dev $intf0 up
+        sudo ip link set dev $intf1 up
 
-apt-get update
-apt-get install -y apache2
+        # Set the MTU of these interfaces to be larger than default of
+        # 1500 bytes, so that P4 behavioral-model testing can be done
+        # on jumbo frames.
+        sudo ip link set $intf0 mtu 9500
+        sudo ip link set $intf1 mtu 9500
 
-cp /vagrant/index.html /var/www/html
+        # Disable IPv6 on the interfaces, so that the Linux kernel
+        # will not automatically send IPv6 MDNS, Router Solicitation,
+        # and Multicast Listener Report packets on the interface,
+        # which can make P4 program debugging more confusing.
+        #
+        # Testing indicates that we can still send IPv6 packets across
+        # such interfaces, both from scapy to simple_switch, and from
+        # simple_switch out to scapy sniffing.
+        #
+        # https://superuser.com/questions/356286/how-can-i-switch-off-ipv6-nd-ra-transmissions-in-linux
+        sudo sysctl net.ipv6.conf.${intf0}.disable_ipv6=1
+        sudo sysctl net.ipv6.conf.${intf1}.disable_ipv6=1
+    fi
+done
