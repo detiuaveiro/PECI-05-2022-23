1bbc01959dc570740220c69b73e9fbbfcb1a17e6
David Araújo <david2araujo5@gmail.com>
2023-04-19 18:26:20 +0100

    Updated vagrant config

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -4,14 +4,17 @@
 sudo apt-get update
 sudo apt-get install -y cmake g++ git automake libtool libgc-dev bison flex libfl-dev libboost-dev libboost-iostreams-dev libboost-graph-dev llvm pkg-config python3 python3-pip tcpdump
 
+curl https://raw.githubusercontent.com/p4lang/p4c/main/requirements.txt > requirements.txt
+pip3 install --user -r requirements.txt
+
 # Install p4c
 . /etc/os-release
 echo "deb https://download.opensuse.org/repositories/home:/p4lang/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/home:p4lang.list
 curl -L "https://download.opensuse.org/repositories/home:/p4lang/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
 sudo apt-get update
-sudo apt install -y p4lang-p4c
+sudo apt install p4lang-p4c
 
 # Install docker and bmv2-switch container
 sudo apt-get install -y docker.io
-docker pull davidjosearaujo/bmv2-switch
-docker run -d --cap-add=NET_ADMIN davidjosearaujo/bmv2-switch --name testSwitch
+sudo docker pull davidjosearaujo/bmv2-switch
+sudo docker run -d --cap-add=NET_ADMIN davidjosearaujo/bmv2-switch --name testSwitch
