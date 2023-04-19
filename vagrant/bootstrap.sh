75cd7dc93847d59e2beaeb7f9402fb2ca6be4e1a
David Araújo <david2araujo5@gmail.com>
2023-04-19 19:48:11 +0100

    Updated vagrant file

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -12,10 +12,10 @@ pip3 install --user -r requirements.txt
 echo "deb https://download.opensuse.org/repositories/home:/p4lang/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/home:p4lang.list
 curl -L "https://download.opensuse.org/repositories/home:/p4lang/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
 sudo apt-get update
-sudo apt install p4lang-p4c
+sudo apt install -y p4lang-p4c
 
 # Install docker and bmv2-switch container
 sudo apt-get install -y docker.io docker-compose
-cd docker/
+cd $HOME/docker/
 sudo docker-compose build
 sudo docker-compose up
