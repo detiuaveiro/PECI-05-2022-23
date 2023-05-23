26a7c56f1e40e6b2c099ba6f491ebd6034afc73f
João Machado <jtomaspm@ua.pt>
2023-05-23 19:48:23 +0100

    updated launcher

--- launcher.py
+++ launcher.py
@@ -1,22 +1,39 @@
 import os
 import subprocess
 
+def launch_backend():
+    app_name = "backend"
+    app_config_path = os.path.join(app_name, "docker-compose.yml")
+    print(f"Launching {app_name}...")
+    subprocess.run(["docker-compose", "-f", app_config_path, "up", "-d"])
+    print("App launched successfully!")
+
+def launch_frontend():
+    try:
+        print("Launching frontend...")
+        os.chdir("./frontend")
+        subprocess.check_call(['npm', 'install'])
+        subprocess.check_call(['npm', 'start'])
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while running the Node.js application: {e}")
+
+def launch_network():
+    pass
+
 APP_CONFIGS = {
-    "App 1": "backend/docker-compose.yml",
-    "App 2": "frontend/docker-compose.yml",
-    # Add more app configurations here
+    "backend": launch_backend,
+    "frontend": launch_frontend,
+    "network": launch_network
 }
+
 
 def display_menu():
     print("=== Launcher ===")
-    print("Required: Docker, Docker Compose, Python 3.6+")
+    print("Required: Docker, Docker Compose, Python 3.6+, node")
     print("Select an app to launch:")
     for idx, app_name in enumerate(APP_CONFIGS.keys(), start=1):
         print(f"{idx}. {app_name}")
     print("0. Exit")
-
-def launch_app(app_config_path):
-    subprocess.run(["docker-compose", "-f", app_config_path, "up", "-d"])
 
 def main():
     while True:
@@ -35,11 +52,8 @@ def main():
             continue
 
         app_name = list(APP_CONFIGS.keys())[choice_idx - 1]
-        app_config_path = APP_CONFIGS[app_name]
+        APP_CONFIGS[app_name]()
 
-        print(f"Launching {app_name}...")
-        launch_app(app_config_path)
-        print("App launched successfully!")
 
 if __name__ == "__main__":
     main()
